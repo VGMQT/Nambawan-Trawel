42b535d047a7e02e49083cd4e7cc98b7716a26f9
Vlad Tepes <vg.email.2@gmail.com>
2018-06-04 17:48:45 +0300

    menu js added

--- src/js/main.js
+++ src/js/main.js
@@ -52,4 +52,84 @@ $(document).ready(function () {
 
     }());
 
+    //----------------------<<menu>>----------------------\\
+    (function () {
+
+        var flag = true;
+
+        $('.menu__btn').on('click', function (e) {
+            e.preventDefault();
+
+            var
+                $this = $(this),
+                menu = $this.closest('.menu'),
+                wrapper = $('.fullpage-wrapper'),
+                list = menu.find('.menu__list'),
+                btnShow = $this.find('.svg_menu'),
+                btnHide = $this.find('.svg_hide'),
+                textShow = $this.find('.menu__btn-menu'),
+                textHide = $this.find('.menu__btn-hide'),
+                socials = menu.find('.menu__socials'),
+                socialsCol = menu.find('.menu__socials_col-wrapper'),
+                durationWrap = 300,
+                durationList = 1500,
+                duration = 500;
+
+            if(flag){
+                flag = false;
+                if(!$this.hasClass('pressed')) {
+
+                    $this.addClass('pressed');
+
+                    menu.animate({
+                        width : 180
+                    }, durationWrap);
+                    wrapper.animate({
+                        marginLeft : 180
+                    }, durationWrap);
+
+                    btnShow.fadeOut(duration, function () {
+                        btnHide.fadeIn(duration);
+                    });
+                    textShow.fadeOut(duration, function () {
+                        textHide.fadeIn(duration);
+                    });
+
+                    list.fadeIn(durationList);
+
+                    socials.fadeOut(duration, function () {
+                        socialsCol.fadeIn(duration,  function () {
+                            flag = true;
+                        });
+                    });
+                } else {
+
+                    $this.removeClass('pressed');
+
+                    menu.animate({
+                        width : 80
+                    }, durationWrap);
+                    wrapper.animate({
+                        marginLeft : 80
+                    }, durationWrap);
+
+                    btnHide.fadeOut(duration, function () {
+                        btnShow.fadeIn(duration);
+                    });
+                    textHide.fadeOut(duration, function () {
+                        textShow.fadeIn(duration);
+                    });
+
+                    list.fadeOut(duration);
+
+                    socialsCol.fadeOut(duration, function () {
+                        socials.fadeIn(duration,  function () {
+                            flag = true;
+                        });
+                    });
+                }
+            }
+        });
+    }());
+
 });
